a16a5c23ed42c097c2e8876e600c7947469f43c5
Joyce <yeechiajoyce@gmail.com>
2021-10-12 10:40:20 +0800

    功能｜懶加載路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-import Homepage from "./components/Homepage";
-import ImageDetail from "./components/ImageDetail";
+import LoadingMask from "./components/LoadingMask";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "./styles/all.scss";
+
+const Homepage = lazy(() => import("./components/Homepage"));
+const ImageDetail = lazy(() => import("./components/ImageDetail"));
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact>
-          <Homepage />
+          <Suspense fallback={<LoadingMask />}>
+            <Homepage />
+          </Suspense>
         </Route>
+
         <Route path="/photo/:id" exact>
-          <ImageDetail />
+          <Suspense fallback={<LoadingMask />}>
+            <ImageDetail />
+          </Suspense>
         </Route>
+
         <Redirect to="/" />
       </Switch>
     </BrowserRouter>
