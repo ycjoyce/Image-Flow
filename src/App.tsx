2962a1610a3aab80c2ec9d92bb4f068f4098c29e
Joyce <yeechiajoyce@gmail.com>
2021-11-04 16:00:35 +0800

    架構｜修改路由根路徑

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,30 @@
 import { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-import { getRootPath } from "./util";
-import LoadingMask from "./components/LoadingMask";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "./styles/all.scss";
+import { getRootPath } from "./util";
+import LoadingMask from "./components/LoadingMask";
 
 const Homepage = lazy(() => import("./components/Homepage"));
 const ImageDetail = lazy(() => import("./components/ImageDetail"));
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={getRootPath(process.env.NODE_ENV)}>
       <Switch>
-        <Route path={getRootPath(process.env.NODE_ENV)} exact>
+        <Route path="/" exact>
           <Suspense fallback={<LoadingMask />}>
             <Homepage />
           </Suspense>
         </Route>
 
-        <Route path={`${getRootPath(process.env.NODE_ENV)}photo/:id`}>
+        <Route path="/photo/:id">
           <Suspense fallback={<LoadingMask />}>
             <ImageDetail />
           </Suspense>
         </Route>
 
-        <Redirect to={getRootPath(process.env.NODE_ENV)} />
+        <Redirect to="/" />
       </Switch>
     </BrowserRouter>
   );
